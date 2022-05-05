48524127bdcf8b76f760719841bf2fffaf099985
Tavelar <tavelor@email.com>
2022-05-05 12:39:05 +0100

    image uploaded working on backend have fun testing

---
+++ backend/routes/image.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+const { postImage, findImageById } = require("../controllers/imageController");
+
+router.post("/", upload.single("productImage"), postImage);
+//
+
+router.get("/:image_id", findImageById);
+module.exports = router;
