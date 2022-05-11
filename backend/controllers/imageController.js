d833913aa0a828c9f568babb600179aab679b6b9
Tavelar <tavelor@email.com>
2022-05-11 10:48:40 +0100

    images post and get

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -5,22 +5,28 @@ const upload = multer({ dest: "uploads/" });
 const Image = require("../schemas/imageSchema");
 
 const postImage = asyncHandler(async (req, res) => {
-  console.log(req.file);
+  // console.log(req);
+  console.log(req.file, "file");
 
   const image = await Image.create({
     name: req.body.name,
     productImage: req.file.path,
   });
-  res.status(200).json(image);
+  console.log(image, "im");
+  res.status(200).send({ image: image });
 });
 
 const findImageById = asyncHandler(async (req, res) => {
-  const image = await Image.findById(req.params.image_id)
-    .select("_id name productImage")
-    .exec();
+  const image = await Image.find({});
+  // .select("_id name productImage")
+  // .exec();
+
+  // const image = await Image.findById(req.params.image_id)
+  // .select("_id name productImage")
+  // .exec();
 
   if (image) {
-    res.status(200).json({ product: image });
+    res.status(200).send({ product: image });
   }
 
   image;
