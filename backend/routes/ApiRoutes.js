6fac1f1133ce7ca5045d2a6872244d86e5b01fd4
Tavelar <tavelor@email.com>
2022-05-17 15:28:42 +0100

    chat modules

--- backend/routes/ApiRoutes.js
+++ backend/routes/ApiRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { getApi } = require("../controllers/");
 const endPoint = require("../../endpoints.json");
 router.get("/", (req, res) => {
   res.status(200).send(endPoint);
